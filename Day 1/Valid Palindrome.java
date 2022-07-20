680ffaef2d1a8986bfb3383d7b13b2005b8d4285
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-20 14:00:00 +0530

    Create Valid Palindrome.java

---
+++ Day 1/Valid Palindrome.java
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/valid-palindrome/
+// using built in functions
+class Solution {
+    public boolean isPalindrome(String s) {
+        String New = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String reverse = new StringBuffer(New).reverse().toString();
+        return New.equals(reverse);
+    }
+}
+
+
+// other Approach
+class Solution {
+    public boolean isPalindrome(String s) {
+        String newString = validPalindrome(s);
+        int start = 0;
+        int end = newString.length()-1;
+        while(start < end){
+            if(newString.charAt(start) != newString.charAt(end)){
+                return false;
+            }
+            else
+                start++;
+            end--;
+        }
+        return true;
+    }
+    public String validPalindrome(String s){
+        String ans = s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+        return ans;
+    }
+}
