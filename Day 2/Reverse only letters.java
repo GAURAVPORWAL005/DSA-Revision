3c6e05cca35cfd445051166bd2288f3817cb8557
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-22 01:05:14 +0530

    Create Reverse only letters.java

---
+++ Day 2/Reverse only letters.java
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/reverse-only-letters/
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        char[] ch = s.toCharArray();
+        while(i < j){
+            if(!Character.isLetter(ch[i])){
+                i++;
+                continue;
+            }
+            if(!Character.isLetter(ch[j])){
+                j--;
+                continue;
+            }
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(ch);
+    }
+}
+time complexity => O(n)
